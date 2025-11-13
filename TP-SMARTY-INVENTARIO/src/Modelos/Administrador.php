4bbbe3298c437ba9dd957cb99df5765bb6e2b997
jmrodev <juanmarcelo.rodrigueztandil@gmail.com>
2025-11-13 17:59:19 -0300

    Corregir clase Administrador: agregar campo DNI y métodos getDni/setDni

--- TP-SMARTY-INVENTARIO/src/Modelos/Administrador.php
+++ TP-SMARTY-INVENTARIO/src/Modelos/Administrador.php
@@ -1,13 +1,26 @@
 <?php
 
-namespace App\Modelos; // Add this line
+namespace App\Modelos;
 
-use App\Modelos\Persona; // Add this line
+use App\Modelos\Persona;
 
 class Administrador extends Persona
 {
-    public function __construct(?int $id, string $nombre, string $username, string $password)
+    protected ?string $dni;
+
+    public function __construct(?int $id, string $nombre, string $username, string $password, ?string $dni = null)
     {
         parent::__construct($id, $nombre, $username, $password, 'admin');
+        $this->dni = $dni;
+    }
+
+    public function getDni(): ?string
+    {
+        return $this->dni;
+    }
+
+    public function setDni(?string $dni): void
+    {
+        $this->dni = $dni;
     }
 }
